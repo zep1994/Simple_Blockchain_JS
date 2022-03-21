51ab1f85cfffceb57e7fd57546d3c5dc8f3fb78c
Thomas Matlock <thomas.matlock@ergon.com>
2022-03-21 08:20:47 -0500

    create time off

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const router = express.Router()
 
+const { route } = require('./admin')
+const BlockChain= require('../BlockChain')
+const Chain = require('../BlockChain')
+
 // Create Home Route
 router.get('/', (req, res) => { 
     console.log("This is the home page")
+})
+
+// Create Time Off Route
+router.post('/time-off', (req, res) => {
+    BlockChain.BlockChain({StartDate: req.body.StartDate, EndDate: req.body.EndDate, Type: req.body.Type, Requesting: req.body.Requesting, Comment: req.body.Comment})
+    res.redirect('/')
 })
 
 module.exports = router
