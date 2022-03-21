3f8d778bee2ef7c72e04468bfd787003f57523a9
Thomas Matlock <thomas.matlock@ergon.com>
2022-03-21 08:41:07 -0500

    Update Time off Form

--- hr_blockchain/src/TimeOff/CreateTimeOff.js
+++ hr_blockchain/src/TimeOff/CreateTimeOff.js
@@ -39,14 +39,32 @@ class TimeOff extends Component {
             <div className="App">
                 <h1 className="Title">Create Time Off</h1>  
                     <form method="POST" action="/time-off" onSubmit={this.handleSubmit}>
-                        <input type="text" name="StartDate" onChange={this.handleChange} placeholder="Start Date" />
+                        <label for="StartDate" name='StartDate'>Start Date</label> <br />
+                        <input type="date" name="StartDate" onChange={this.handleChange} placeholder="Start Date" />
                         <br />
-                        <input type="text" name="EndDate" onChange={this.handleChange} placeholder='Emd Date' />
+
+                        <label for="EndDate" name='EndDate'>End Date</label> <br />
+                        <input type="date" name="EndDate" onChange={this.handleChange} placeholder='Emd Date' />
                         <br />
-                        <input type="text" name="Type" onChange={this.handleChange} placeholder='Type' />
                         <br />
-                        <input type="text" name="Requesting" onChange={this.handleChange} placeholder='Requesting' />
+
+                        <label for="Type" name='Type'>Type of Time Off</label> <br />
+                        <select name="Type" id="type" onChange={this.handleChange}>
+                            <option value="Vacation">Vacation</option>
+                            <option value="Medical Leave Paid">Meidcal Leave Paid</option>
+                        </select>
                         <br />
+
+                        <select name="Requesting" id="requesting" onChange={this.handleChange}>
+                            <option value="Half Day (Morning)">Half Day (Morning)</option>
+                            <option value="Half Day (Afternoon)">Half Day (Afternoon)</option>
+                            <option value="Full Day">Full Day</option>
+                        </select>
+                        {/* <input type="text" name="Type" onChange={this.handleChange} placeholder='Type' /> */}
+                        <br />
+                        {/* <input type="text" name="Requesting" onChange={this.handleChange} placeholder='Requesting' /> */}
+                        <br />
+                        <label for="Comment" name='Comment'>Comment</label> <br />
                         <input type="text" name="Comment" onChange={this.handleChange} placeholder='Comment' />
                         <br />
                         <input type="submit" value="Submit"/>
