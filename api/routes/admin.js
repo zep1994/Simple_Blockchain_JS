ffae1180de1cf7e145d76452b5246e7e19a6f2c7
Thomas Matlock <thomas.matlock@ergon.com>
2022-05-11 15:09:01 -0500

    Add route protection

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -3,12 +3,12 @@ const router = express.Router()
 
 const EmployeeController = require('../Controllers/EmployeesController')
 const AdminController = require('../Controllers/AdminController')
-
+const isAuth = require('../Middleware/is-auth')
 
 // Admin Employee Routes
-router.get('/add-employee', EmployeeController.getAddEmployee)
-router.post('/employee', EmployeeController.postAddEmployee)
-router.get('/employees', EmployeeController.getAllEmployees)
+router.get('/add-employee', isAuth, EmployeeController.getAddEmployee)
+router.post('/employee', isAuth, EmployeeController.postAddEmployee)
+router.get('/employees', isAuth, EmployeeController.getAllEmployees)
 
 module.exports = router 
 
--- api/routes/employee.js
+++ api/routes/employee.js
@@ -2,18 +2,19 @@ const express = require('express')
 const router = express.Router()
 
 const TimeOffController = require('../Controllers/TimeOffController')
+const isAuth = require('../Middleware/is-auth')
 
 // Master Time Off
-router.get('/timeoff_master', TimeOffController.getTimesOff)
+router.get('/timeoff_master', isAuth, TimeOffController.getTimesOff)
 
 // GET Time off Route
-router.get('/timeoff', TimeOffController.getTimeOff)
+router.get('/timeoff', isAuth, TimeOffController.getTimeOff)
 
 // POST Sign up Route
-router.post('/timeoff', TimeOffController.postTimeOff)
+router.post('/timeoff', isAuth, TimeOffController.postTimeOff)
 
 //GET SINGLE TIME
-router.get('/timeoff/:timeId', TimeOffController.getTime)
+router.get('/timeoff/:timeId', isAuth, TimeOffController.getTime)
 
 module.exports = router
 
